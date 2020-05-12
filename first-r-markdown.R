f5e046951fd40160aa2e8c3e8931fdd714567d42
Brandon <brandon@thefarrs.org>
2020-05-11 21:27:32 -0400

    Created an r script analog to the rmd file

---
+++ first-r-markdown.R
@@ -0,0 +1,20 @@
+#' ---
+#' title: "First R Markdown"
+#' author: "BCF"
+#' date: "5/11/2020"
+#' output: github_document
+#' ---
+
+#' ```{r setup, include=FALSE}
+#' knitr::opts_chunk$set(echo = TRUE)
+#' ```
+
+#' ## R Markdown
+
+#+ r
+
+library(dplyr)
+head(mtcars)
+
+#+
+
